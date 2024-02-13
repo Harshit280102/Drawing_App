9ce9e30bc6a4cced89d31bffc013f5077ba0c6f3
Harshit Joshi <harshitjoshi2002@gmail.com>
2024-02-13 04:10:04 -0800

    Private and Group Chat Implemented in The Backend no standard code till now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { io } from 'socket.io-client'
+import {Box, Button, Container, Stack, TextField, Typography} from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const socket =io("http://localhost:4000");
+
+  const [message,setMessage]=useState("");
+  const [room,setRoom]=useState("");
+  const [socketID,setSocketID] =useState<string|undefined>("");
+  const [messages,setMessages] =useState<string[]|undefined>([]);
+
+
+  const handleSubmit = (e:any)=>{
+    e.preventDefault();
+    socket.emit("message",{message,room});
+    setMessage("");
+  }
+
+  useEffect(()=>{
+    socket.on("connect",()=>{
+      setSocketID(socket.id);
+       console.log("connected",socket.id);
+    })
+
+    socket.on('welcome',(response:any)=>{
+       console.log(response);
+    })
+
+    socket.on('recived-message',(data)=>{
+      setMessages((prv:any)=>[...prv,data])
+      console.log(data)
+      
+    })
+
+    return()=>{
+      socket.disconnect();
+    }
+  },[])
+
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth="sm">
+      <Box sx={{height:500}}/>
+      <Typography variant='h1' component='div' gutterBottom>
+        Welcome to This App
+      </Typography>
+      <Typography variant='h6' component='div' gutterBottom>
+          {socketID}
+      </Typography>
+      <form onSubmit={handleSubmit}>
+      <TextField 
+        id='outlined-basic' 
+        label="Room" 
+        variant="outlined" 
+        value={room}
+        onChange={(e)=>setRoom(e.target.value)}
+        />
+        <TextField 
+        id='outlined-basic' 
+        label="Message" 
+        variant="outlined" 
+        value={message}
+        onChange={(e)=>setMessage(e.target.value)}
+        />
+        <Button variant='contained' color='primary' type='submit'>Send</Button>
+      </form>
+     <Stack>
+      {
+        messages?.map((m,i)=>(
+          <Typography key={i} variant='h6' component='div' gutterBottom>
+        {m}
+      </Typography>
+        ))
+      }
+     </Stack>
+    </Container>
   )
 }
 
--- server/index.ts
+++ server/index.ts
@@ -3,18 +3,48 @@ import {Server} from "socket.io"
 import dotenv from 'dotenv';
 import path from "path";
 import http from "http";
+import cors from 'cors';
 
 
 dotenv.config();
 const app: Application = express();
 
+
+
+app.use(cors({
+  origin:"http://localhost:5173",
+  methods:["GET","POST"],
+  credentials:true,
+}))
+
+
+
 //Socket Setup
 const server =http.createServer(app);  //using a http module to create HTTP Server
-const io = new Server(server);      //creating a new Socket.io server
+const io = new Server(server,{
+  cors:{
+    origin:"http://localhost:5173",
+    methods:["GET","POST"],
+    credentials:true,
+  }
+});      //creating a new Socket.io server
 
 //Socket io requests handed by socket server
-io.on("connection",(client_socket)=>{
-   console.log("A client has added into the socket server", client_socket.id)
+io.on("connection",(socket)=>{
+   socket.emit("welcome",`welcome message from server ${socket.id}`);
+
+
+   socket.on('message',(data)=>{
+    console.log(`message from user${socket.id}:`,data);
+    // io.emit("recived-message",data);
+    io.to(data.room).emit("recived-message",data.message);
+   })
+
+
+
+   socket.on("disconnet",()=>{
+    console.log("user Disconnected",socket.id)
+   })
 });
 
 
@@ -23,17 +53,17 @@ io.on("connection",(client_socket)=>{
 
 //Restful Requests handed by express server
 const port = process.env.PORT_NO || 8000;
-app.use(express.static(path.resolve("./public")));
+// app.use(express.static(path.resolve("./public")));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to My Real-Time Drawing and Chatting App Server');
 });
 
-app.get('/chat',(req: Request, res: Response)=>{
- console.log("running")
- return res.sendFile(path.resolve("./public/index.html"));
-})
+// app.get('/chat',(req: Request, res: Response)=>{
+//  console.log("running")
+//  return res.sendFile(path.resolve("./public/index.html"));
+// })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
